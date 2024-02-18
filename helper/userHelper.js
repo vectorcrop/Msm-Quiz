8b442e0b4f99365faa5584d40a1810d4cfc27f19
Shabeer <shabeeralavi001@gmail.com>
2024-02-18 14:12:23 +0530

    just remove razorpay pakage

--- helper/userHelper.js
+++ helper/userHelper.js
@@ -2,12 +2,12 @@ var db = require("../config/connection");
 var collections = require("../config/collections");
 const bcrypt = require("bcrypt");
 const objectId = require("mongodb").ObjectID;
-const Razorpay = require("razorpay");
+// const Razorpay = require("razorpay");
 
-var instance = new Razorpay({
-  key_id: "rzp_test_8NokNgt8cA3Hdv",
-  key_secret: "xPzG53EXxT8PKr34qT7CTFm9",
-});
+// var instance = new Razorpay({
+//   key_id: "rzp_test_8NokNgt8cA3Hdv",
+//   key_secret: "xPzG53EXxT8PKr34qT7CTFm9",
+// });
 
 module.exports = {
 
